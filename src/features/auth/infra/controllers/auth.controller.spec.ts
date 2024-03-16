cc748fddc6191f5955f24cedc349fe9e719a0844
Jônatas Alves <49556955+JonatasAlves9@users.noreply.github.com>
2024-03-16 19:14:32 -0300

    test: put imports of ID

--- src/features/auth/infra/controllers/auth.controller.spec.ts
+++ src/features/auth/infra/controllers/auth.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from '../../application/auth.service';
+import { UsersModule } from '../../../../features/users/users.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -9,6 +10,7 @@ describe('AuthController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [AuthService],
+      imports: [UsersModule],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
