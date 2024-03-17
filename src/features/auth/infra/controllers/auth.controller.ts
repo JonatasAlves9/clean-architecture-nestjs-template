71ecc6e29c80df183960eda6421b65ebbd45c147
Jônatas Alves <49556955+JonatasAlves9@users.noreply.github.com>
2024-03-16 22:24:52 -0300

    feat: create public routes

--- src/features/auth/infra/controllers/auth.controller.ts
+++ src/features/auth/infra/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../../application/auth.service';
 import { SignInDTO } from '../../domain/dto/create-auth.dto';
+import { Public } from '@shared/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signin')
   signIn(@Body() createAuthDto: SignInDTO) {
     return this.authService.signIn(createAuthDto.username, createAuthDto.pass);
